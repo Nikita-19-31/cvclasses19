5168750a79077e91a2c12b8f7e4a0ded48f9b9fd
Nikita-19-31 <tyurenkovn@mail.ru>
2023-12-07 23:13:50 +0300

    lab6: Выполнено.

--- cvlib/src/descriptor_matcher.cpp
+++ cvlib/src/descriptor_matcher.cpp
@@ -16,15 +16,37 @@ void descriptor_matcher::knnMatchImpl(cv::InputArray queryDescriptors, std::vect
 
     auto q_desc = queryDescriptors.getMat();
     auto& t_desc = trainDescCollection[0];
+    int current_dist;
+    int _trainIdx;
+    int _distance = ratio_;
+    bool flag;
 
     matches.resize(q_desc.rows);
 
     cv::RNG rnd;
     for (int i = 0; i < q_desc.rows; ++i)
     {
+        _trainIdx = 0;
+        _distance = ratio_;
+        flag = false;
+
         // \todo implement Ratio of SSD check.
-        matches[i].emplace_back(i, rnd.uniform(0, t_desc.rows), FLT_MAX);
+        for (int j = 0; j < t_desc.rows; j++)
+        {
+            current_dist = distance(q_desc.row(i), t_desc.row(j));
+
+            if (current_dist < _distance)
+            {
+                flag = true;
+                _distance = current_dist;
+                _trainIdx = j;
+            }
+        }
+
+        if (flag)
+            matches[i].emplace_back(i, _trainIdx, _distance);
     }
+
 }
 
 void descriptor_matcher::radiusMatchImpl(cv::InputArray queryDescriptors, std::vector<std::vector<cv::DMatch>>& matches, float /*maxDistance*/,
@@ -32,5 +54,31 @@ void descriptor_matcher::radiusMatchImpl(cv::InputArray queryDescriptors, std::v
 {
     // \todo implement matching with "maxDistance"
     knnMatchImpl(queryDescriptors, matches, 1, masks, compactResult);
+}
+
+int descriptor_matcher::distance(const cv::Mat& q_desc, const cv::Mat& t_desc)
+{
+    int dist = 0;
+    int length = q_desc.cols;
+    uint16_t bytes_q, bytes_t;
+    bool bit_q, bit_t;
+
+    for (int i = 0; i < length; ++i)
+    {
+        bytes_q = q_desc.at<uint16_t>(0, i);
+        bytes_t = t_desc.at<uint16_t>(0, i);
+
+        for (int j = 0; j < 16; ++j)
+        {
+            bit_q = (((bytes_q) << (15 - j))&0xFFFF) >> 15;
+            bit_t = (((bytes_t) << (15 - j))&0xFFFF) >> 15;
+
+            if (bit_q != bit_t)
+                dist++;
+        }
+
+    }
+
+    return dist;
 }
 } // namespace cvlib
